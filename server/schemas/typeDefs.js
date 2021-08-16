709b5b258a5585f64c7171f215885da24855ac06
Ronald Main <rbmain84@yahoo.com>
2021-08-16 11:20:30 -0500

    add schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  input saveBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: saveBook!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
